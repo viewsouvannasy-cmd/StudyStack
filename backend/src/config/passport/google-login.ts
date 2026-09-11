bc8d308e6014cbd26fcf51a6573b1cb280bfd5d1
view <viewsouvannasy@gmail.com>
2026-09-11 16:03:26 +0700

    feat: implement Google OAuth login with state management and email validation

--- backend/src/config/passport/google-login.ts
+++ backend/src/config/passport/google-login.ts
@@ -1,7 +1,11 @@
+// library
 import passport from "passport";
 import { Strategy as GoogleStrategy, Profile } from "passport-google-oauth20";
-import { getEnv } from "../../utils/getEnv.js";
 import { sql } from "../database.js";
+
+// helper function
+import { getEnv } from "../../utils/getEnv.js";
+import { generateUniqueUsername } from "../../utils/generate.js";
 
 passport.use(
   new GoogleStrategy(
@@ -17,10 +21,15 @@ passport.use(
       done: (error: unknown, user?: any) => void,
     ) => {
       try {
+        const email = profile.emails?.[0]?.value ?? "";
+        if (!email) {
+          return done(new Error("Google profile did not provide an email"));
+        }
+
         const userData = {
           id: profile.id,
           name: profile.displayName,
-          email: profile.emails?.[0]?.value ?? "",
+          email,
           avatarUrl: profile.photos?.[0]?.value ?? "",
         };
 
@@ -43,10 +52,24 @@ passport.use(
           return done(null, oldUser);
         }
 
+        let uniqueName = userData.name;
+
+        // check duplicate name
+        const isDuplicatrName = await sql`
+        SELECT 
+        * 
+        FROM users 
+        WHERE user_name = ${uniqueName}
+        `;
+        if (isDuplicatrName.length > 0) {
+          // generate unqiue name
+          uniqueName = await generateUniqueUsername(uniqueName);
+        }
+
         const [newUser] = await sql`
         INSERT INTO users(user_name, user_email, provider , profile_url)
         VALUES (
-        ${userData.name},
+        ${uniqueName},
         ${userData.email},
         'google',
         ${userData.avatarUrl}
