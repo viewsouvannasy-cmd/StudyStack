6f74462b44698b77b397e592e963c69773d33dd2
view <viewsouvannasy@gmail.com>
2026-09-20 20:49:41 +0700

    feat: refactor route components to integrate useOpenPopup and improve body overflow handling

--- frontend/src/routes/app/notebook.tsx
+++ frontend/src/routes/app/notebook.tsx
@@ -8,11 +8,28 @@ import { TabSection } from "../../components/app/TabSection";
 import { DisplayItemSection } from "../../components/app/DisplayItemSection";
 import { PopupAddSource } from "../../components/popup/pop-add-source/PopupAddSource";
 
+// context
+import useOpenPopup from "../../context/useOpenPopup";
+
 export const Route = createFileRoute("/app/notebook")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const { isOpen } = useOpenPopup();
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "unset";
+    }
+
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, [isOpen]);
+
   useEffect(() => {
     document.title = "StudyStack | My Learning";
   });
